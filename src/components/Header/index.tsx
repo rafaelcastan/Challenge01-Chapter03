337fb5fcb25da38d17d9237c1aca0a9abaac7865
rafael <rafael@pop-os.localdomain>
2021-07-01 18:00:36 -0300

    Add Button Function and Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
-export default function Header() {
-  // TODO
+import { ReactElement } from 'react';
+import styles from './header.module.scss';
+
+export default function Header(): ReactElement {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <a href="/">
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import Prismic from '@prismicio/client';
-import { ReactChild } from 'react';
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import { useState } from 'react';
+import ApiSearchResponse from '@prismicio/client/types/ApiSearchResponse';
+import { ReactElement } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -28,15 +30,45 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({ postsPagination }: HomeProps): ReactChild {
+export default function Home({ postsPagination }: HomeProps): ReactElement {
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+
+  function LoadMorePosts(): void {
+    fetch(nextPage).then(async function fetchResponse(response) {
+      const responseResolved: ApiSearchResponse = await Promise.resolve(
+        response.json()
+      );
+      const results = responseResolved.results.map(post => {
+        return {
+          uid: post.uid,
+          first_publication_date: new Date(
+            post.last_publication_date
+          ).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric',
+          }),
+          data: {
+            title: post.data.title,
+            subtitle: post.data.subtitle,
+            author: post.data.author,
+          },
+        };
+      });
+      const { next_page } = responseResolved;
+      setPosts([...posts, ...results]);
+      setNextPage(next_page);
+    });
+  }
+
   return (
     <>
       <Head>
         <title>Posts | Ignews</title>
       </Head>
       <main className={styles.container}>
-        <img src="logo.svg" alt="logo" />
-        {postsPagination.results.map(post => (
+        {posts.map(post => (
           <div className={styles.posts} key={post.uid}>
             <a href={`/posts/${post.uid}`}>
               <strong>{post.data.title}</strong>
@@ -54,8 +86,10 @@ export default function Home({ postsPagination }: HomeProps): ReactChild {
             </div>
           </div>
         ))}
-        {postsPagination.next_page && (
-          <button type="button">Carregar mais posts</button>
+        {nextPage && (
+          <button onClick={LoadMorePosts} type="button">
+            Carregar mais posts
+          </button>
         )}
       </main>
     </>
